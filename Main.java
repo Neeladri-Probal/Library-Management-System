289f82f4bc4f0ff9416724af9578dc21b1158a3b
NeeladriProbal <cse_0182320012101268@lus.ac.bd>
2025-08-24 17:41:14 +0600

    added case-3 search book

--- Main.java
+++ Main.java
@@ -1,29 +1,28 @@
 
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Central Library");
         Scanner sc = new Scanner(System.in);
-        while(true)
-        {
-            System.out.println("1.Add book");
-            System.out.println("2.View books");
-            System.out.println("3.Search book");
-            System.out.println("4.Add member");
-            System.out.println("5.View Member");
-            System.out.println("6.Borrow book");
-            System.out.println("7.Return book");
-            System.out.println("8.View Borrowed book");
-            System.out.println("9.Exit\n");
+        System.out.println("1.Add book");
+        System.out.println("2.View books");
+        System.out.println("3.Search book");
+        System.out.println("4.Add member");
+        System.out.println("5.View Member");
+        System.out.println("6.Borrow book");
+        System.out.println("7.Return book");
+        System.out.println("8.View Borrowed book");
+        System.out.println("9.Exit\n");
+        while (true) {
+
             System.out.print("Choose option: ");
 
             int choice = sc.nextInt();
 
             switch (choice) {
-                case 1:
-                {
+                case 1: {
                     System.out.print("Book ID: ");
                     Scanner sc1 = new Scanner(System.in);
                     int bookId = sc1.nextInt();
@@ -33,21 +32,25 @@ public class Main{
                     System.out.print("Author: ");
                     sc1 = new Scanner(System.in);
                     String author = sc1.nextLine();
-                    Book book = new Book(bookId,title,author);
+                    Book book = new Book(bookId, title, author);
                     Management.addBook(book);
                     System.out.println("Book added successfully.\n");
                     break;
                 }
-                case 2:
-                {
-                    if(!Management.showBooks())
-                    {
+                case 2: {
+                    if (!Management.showBooks()) {
                         System.out.println("No book available.");
                     }
                     break;
                 }
-                case 9:
-                {
+                case 3: {
+                    System.out.print("Enter Book name: ");
+                    Scanner sc1 = new Scanner(System.in);
+                    String s = sc1.nextLine();
+                    Management.searchbook(s);
+                    break;
+                }
+                case 9: {
                     System.out.println("Thanks for visiting...");
                     return;
                 }
--- Management.java
+++ Management.java
@@ -1,21 +1,33 @@
 import java.util.ArrayList;
+
 public class Management {
     static ArrayList<Book> books = new ArrayList<>();
-    static void addBook(Book b)
-    {
+
+    static void addBook(Book b) {
         books.add(b);
     }
-    static boolean showBooks()
-    {
-        if(books.isEmpty())
+
+    static boolean showBooks() {
+        if (books.isEmpty())
             return false;
-        else
-        {
-            for(Book book:books)
-            {
+        else {
+            for (Book book : books) {
                 System.out.println(book.title);
             }
         }
         return true;
+    }
+
+    static void searchbook(String s) {
+        boolean flag = false;
+        for (Book book : books) {
+            if(book.title.equals(s))
+            {
+                System.out.println("Available...");
+                flag = true;
+            }
+        }
+        if (!flag)
+            System.out.println("Not Available...");
     }
 }
