80b69c160eb70619d69db7e1c96099d16ba26745
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-09-08 01:20:57 +0600

    update borrow book logic with fine implementaion

--- Book.java
+++ Book.java
@@ -1,25 +1,13 @@
-import java.time.LocalDate;
-
 public class Book {
     int bookId;
     String title;
     String author;
     String genre;
-    boolean isBorrowed = false;
-
-    LocalDate borrowDate;  
-    LocalDate dueDate;    
-
-    public Book(int bookId, String title, String author, String genre) {
+    Book(int bookId,String title,String author,String genre)
+    {
         this.bookId = bookId;
         this.title = title;
         this.author = author;
         this.genre = genre;
-    }
-
-    @Override
-    public String toString() {
-        return bookId + " | " + title + " | " + author + " | " + genre +
-               (isBorrowed ? " [Borrowed, Due: " + dueDate + "]" : " [Available]");
     }
 }
